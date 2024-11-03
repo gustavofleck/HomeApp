64caac434e542eef7e61ab707c04e940ff1b99ea
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-03 15:01:52 -0300

    FEAT: Initialize koin modules with the application

--- app/src/main/java/com/example/homeapp/application/HomeApp.kt
+++ app/src/main/java/com/example/homeapp/application/HomeApp.kt
@@ -1,6 +1,8 @@
 package com.example.homeapp.application
 
 import android.app.Application
+import com.example.homeapp.annotations.di.annotationModule
+import com.example.homeapp.login.di.loginModule
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
 import org.koin.android.ext.koin.androidContext
@@ -15,7 +17,7 @@ class HomeApp : Application() {
         startKoin {
             androidLogger()
             androidContext(this@HomeApp)
-            modules()
+            modules(loginModule + annotationModule)
         }
     }
 
