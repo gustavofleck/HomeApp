dba1e2ab5051917adb29240363b262586cc7c22e
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-03 12:16:59 -0300

    FEAT: Add koin initialization

--- app/src/main/java/com/example/homeapp/application/HomeApp.kt
+++ app/src/main/java/com/example/homeapp/application/HomeApp.kt
@@ -3,12 +3,20 @@ package com.example.homeapp.application
 import android.app.Application
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class HomeApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Firebase.initialize(this)
+        startKoin {
+            androidLogger()
+            androidContext(this@HomeApp)
+            modules()
+        }
     }
 
 }
