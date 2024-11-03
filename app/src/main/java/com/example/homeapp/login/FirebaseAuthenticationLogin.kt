fd9a1cacad1597e17187c6d5ee9c1235471c714e
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-03 10:44:48 -0300

    FEAT: Add Firebase login activity

--- app/src/main/java/com/example/homeapp/login/FirebaseAuthenticationLogin.kt
+++ app/src/main/java/com/example/homeapp/login/FirebaseAuthenticationLogin.kt
@@ -1,12 +1,14 @@
 package com.example.homeapp.login
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,53 +18,57 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ds.components.DSHeader
+import com.example.ds.components.DSInputText
 import com.example.ds.components.DSPrimaryButton
 import com.example.ds.theme.DesignSystemTheme
 import com.example.homeapp.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-
-const val RC_SIGN_IN = 9001
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class FirebaseAuthenticationLogin : AppCompatActivity() {
 
-    private lateinit var googleSignInClient: GoogleSignInClient
+    private val auth: FirebaseAuth by lazy { Firebase.auth }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setupGoogleSignIn()
-
         enableEdgeToEdge()
         setContent {
             DesignSystemTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Login(modifier = Modifier.padding(innerPadding)) {
-                        val signInIntent = googleSignInClient.signInIntent
-                        startActivityForResult(signInIntent, RC_SIGN_IN)
+                        setupSignIn()
                     }
                 }
             }
         }
-
     }
 
-    private fun setupGoogleSignIn() {
-        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("487875437493-r3qgd9sen877nnq32t40v6pgnvg7j3p4.apps.googleusercontent.com")
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+    private fun setupSignIn() {
+        auth.signInWithEmailAndPassword("gustavocfleck@gmail.com", "rytxMF7M_TLZ_XQuvyTw")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Signed in as ${user?.displayName}", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
+
 }
 
 @Composable
 fun Login(modifier: Modifier = Modifier, signIn: () -> Unit) {
     Column(modifier = modifier.fillMaxHeight(), verticalArrangement = Arrangement.SpaceBetween) {
         DSHeader(title = "Home App") {}
-        Image(painterResource(id = R.drawable.home_app_launcher_icon), contentDescription = "Home App Launcher Icon")
+        Row {
+            Column(verticalArrangement = Arrangement.SpaceBetween) {
+                Image(painterResource(id = R.drawable.home_app_launcher_icon), contentDescription = null)
+                DSInputText(placeholder = "Email")
+                DSInputText(placeholder = "Password")
+            }
+        }
         DSPrimaryButton(text = "Sign in", signIn)
     }
 }
