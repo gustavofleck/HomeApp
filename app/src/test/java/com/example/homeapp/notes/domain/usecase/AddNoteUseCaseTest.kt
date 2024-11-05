ed4bfd919c9cfa1ac9e13de77049814dd9d9dab1
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-05 15:07:21 -0300

    FEAT: Add AddNoteUseCase unit tests

---
+++ app/src/test/java/com/example/homeapp/notes/domain/usecase/AddNoteUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.example.homeapp.notes.domain.usecase
+
+import com.example.homeapp.notes.domain.repository.NotesRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class AddNoteUseCaseTest {
+
+    private val text = "text"
+    private val favorite = true
+    private val repository = mockk<NotesRepository>()
+    private val useCase = AddNoteUseCase(repository)
+
+    @Test
+    fun `invoke Should return a Result Success When repository returns Result Success`() = runBlocking {
+        val expectedResult = Result.success(Unit)
+        coEvery { repository.addNote(text, favorite) } returns expectedResult
+
+        val result = useCase.invoke(text, favorite)
+
+        assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun `invoke Should return a Result Failure When repository returns Result Failure`() = runBlocking {
+        val expectedResult = Result.failure<Unit>(Exception())
+        coEvery { repository.addNote(text, favorite) } returns expectedResult
+
+        val result = useCase.invoke(text, favorite)
+
+        assertEquals(expectedResult, result)
+    }
+}
