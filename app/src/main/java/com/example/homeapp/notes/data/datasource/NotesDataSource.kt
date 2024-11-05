27f1e502ab0eadcd266813a1803a01b152cd01d0
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-05 15:24:53 -0300

    FEAT: Add addNote datasource method

--- app/src/main/java/com/example/homeapp/notes/data/datasource/NotesDataSource.kt
+++ app/src/main/java/com/example/homeapp/notes/data/datasource/NotesDataSource.kt
@@ -1,5 +1,6 @@
 package com.example.homeapp.notes.data.datasource
 
+import com.example.homeapp.notes.data.model.NoteRequest
 import com.example.homeapp.notes.data.model.toDomain
 import com.example.homeapp.notes.data.service.NotesApi
 import com.example.homeapp.notes.domain.model.Note
@@ -12,6 +13,12 @@ internal class NotesDataSource(
         return api.notes().documents?.map { annotationResponse ->
             annotationResponse.toDomain()
         }.orEmpty()
+    }
+
+    suspend fun addNote(note: NoteRequest): Result<Unit> {
+        return runCatching {
+            api.addNote(note)
+        }
     }
 
 }
--- app/src/test/java/com/example/homeapp/notes/data/datasource/NotesDataSourceTest.kt
+++ app/src/test/java/com/example/homeapp/notes/data/datasource/NotesDataSourceTest.kt
@@ -1,5 +1,6 @@
 package com.example.homeapp.notes.data.datasource
 
+import com.example.homeapp.notes.data.model.NoteRequest
 import com.example.homeapp.notes.data.service.NotesApi
 import com.example.homeapp.stubs.noteList
 import com.example.homeapp.stubs.noteListResponse
@@ -17,17 +18,18 @@ internal class NotesDataSourceTest {
     private val dataSource = NotesDataSource(api)
 
     @Test
-    fun `annotations Should return a list of annotations When api returns data with success`() = runBlocking {
-        val expectedAnnotations = noteList
-        coEvery { api.notes() } returns noteListResponse
+    fun `notes Should return a list of annotations When api returns data with success`() =
+        runBlocking {
+            val expectedAnnotations = noteList
+            coEvery { api.notes() } returns noteListResponse
 
-        val result = dataSource.notes()
+            val result = dataSource.notes()
 
-        assertEquals(expectedAnnotations, result)
-    }
+            assertEquals(expectedAnnotations, result)
+        }
 
     @Test
-    fun `annotations Should return an empty list When api returns null data`() = runBlocking {
+    fun `notes Should return an empty list When api returns null data`() = runBlocking {
         val expectedAnnotations = emptyList<String>()
         coEvery { api.notes() } returns nullAnnotationListResponse
 
@@ -37,7 +39,7 @@ internal class NotesDataSourceTest {
     }
 
     @Test
-    fun `annotations Should return an empty list When api throws an exception`() {
+    fun `notes Should return an empty list When api throws an exception`() {
         coEvery { api.notes() } throws Exception()
 
         assertThrows(Exception::class.java) {
@@ -45,5 +47,26 @@ internal class NotesDataSourceTest {
                 dataSource.notes()
             }
         }
+    }
+
+    @Test
+    fun `addNote Should return a Success result When api returns success`() = runBlocking {
+        val request = mockk<NoteRequest>()
+        coEvery { api.addNote(request) } returns Unit
+
+        val result = dataSource.addNote(request)
+
+        assertEquals(Result.success(Unit), result)
+    }
+
+    @Test
+    fun `addNote Should return a Failure result When api throws an exception`() = runBlocking {
+        val request = mockk<NoteRequest>()
+        val expectedException = Exception()
+        coEvery { api.addNote(request) } throws expectedException
+
+        val result = dataSource.addNote(request)
+
+        assertEquals(Result.failure<Unit>(expectedException), result)
     }
 }
