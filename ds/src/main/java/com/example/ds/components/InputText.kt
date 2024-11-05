0e552c9618f140a782a1bbc623e983ab602d311d
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-05 15:27:09 -0300

    UPDATE: Update input text to define min and max lines, and add a new password input text

--- ds/src/main/java/com/example/ds/components/InputText.kt
+++ ds/src/main/java/com/example/ds/components/InputText.kt
@@ -2,23 +2,22 @@ package com.example.ds.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ds.theme.primary
 import com.example.ds.theme.secondary
 import com.example.ds.theme.tertiary
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DSInputText(
     modifier: Modifier = Modifier.fillMaxWidth().padding(16.dp),
@@ -26,7 +25,9 @@ fun DSInputText(
     onValueChange: (String) -> Unit = {},
     label: String? = null,
     placeholder: String? = null,
-    isError: Boolean = false
+    isError: Boolean = false,
+    maxLines: Int = 1,
+    minLines: Int = 1
 ) {
 
     var text by remember { mutableStateOf(value) }
@@ -39,17 +40,49 @@ fun DSInputText(
         },
         label = { label?.let { Text(text = label)}},
         isError = isError,
-        colors = TextFieldDefaults.outlinedTextFieldColors(
+        colors = OutlinedTextFieldDefaults.colors(
             focusedBorderColor = if (isError) secondary else primary,
-            unfocusedBorderColor = if (isError) tertiary else secondary
+            unfocusedBorderColor = if (isError) tertiary else secondary,
+            unfocusedPlaceholderColor = secondary
         ),
         placeholder = { placeholder?.let { Text(text = placeholder) } },
-        modifier = modifier
+        modifier = modifier,
+        maxLines = maxLines,
+        minLines = minLines
+    )
+}
+
+@Composable
+fun DSPasswordInputText(
+    modifier: Modifier = Modifier.fillMaxWidth().padding(16.dp),
+    value: String = "",
+    onValueChange: (String) -> Unit = {},
+    isError: Boolean = false
+) {
+
+    var text by remember { mutableStateOf(value) }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = {
+            text = it
+            onValueChange(it)
+        },
+        label = { Text(text = "Password")},
+        isError = isError,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = if (isError) secondary else primary,
+            unfocusedBorderColor = if (isError) tertiary else secondary,
+            unfocusedPlaceholderColor = secondary
+        ),
+        placeholder = { Text(text = "Password") },
+        modifier = modifier,
+        visualTransformation = PasswordVisualTransformation()
     )
 }
 
 @Preview
 @Composable
 fun DSInputTextPreview() {
-    DSInputText(value = "Hello", onValueChange = {}, isError = true)
+    DSInputText(value = "Hello", onValueChange = {}, isError = false)
 }
