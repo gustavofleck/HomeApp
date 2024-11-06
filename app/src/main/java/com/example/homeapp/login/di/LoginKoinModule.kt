f104cc8c428d1a6fee32b8a586b312f01add903f
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-06 13:11:05 -0300

    FEAT: Add dependencies into login koin module

--- app/src/main/java/com/example/homeapp/login/di/LoginKoinModule.kt
+++ app/src/main/java/com/example/homeapp/login/di/LoginKoinModule.kt
@@ -1,7 +1,17 @@
 package com.example.homeapp.login.di
 
+import com.example.homeapp.login.presentation.viewmodel.LoginViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
-val loginModule = module {
-
+val presentationModule = module {
+    viewModel {
+        LoginViewModel(
+            auth = Firebase.auth
+        )
+    }
 }
+
+val loginModule = listOf(presentationModule)
