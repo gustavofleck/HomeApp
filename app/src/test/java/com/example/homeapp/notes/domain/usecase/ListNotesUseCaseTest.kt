31158076ee83ff7e41114922de7681873e8b7b5d
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-06 11:15:15 -0300

    FIX: Adjust broken unit test

--- app/src/test/java/com/example/homeapp/notes/domain/usecase/ListNotesUseCaseTest.kt
+++ app/src/test/java/com/example/homeapp/notes/domain/usecase/ListNotesUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.example.homeapp.notes.domain.usecase
 
+import com.example.homeapp.notes.domain.model.Note
 import com.example.homeapp.notes.domain.repository.NotesRepository
 import com.example.homeapp.stubs.noteList
 import com.example.homeapp.stubs.orderedNoteList
@@ -7,31 +8,32 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThrows
 import org.junit.Test
 
 internal class ListNotesUseCaseTest {
 
-    private val repository = mockk<NotesRepository>()
+    private val repository = mockk<NotesRepository>(relaxed = true)
     private val useCase = ListNotesUseCase(repository)
 
     @Test
     fun `invoke Should return ordered annotations`() = runBlocking {
-        val expectedAnnotations = orderedNoteList
+        val expectedResult = Result.success(orderedNoteList)
         coEvery { repository.notes() } returns noteList
 
         val result = useCase()
 
-        assertEquals(expectedAnnotations, result)
+        assertEquals(expectedResult, result)
     }
 
     @Test
-    fun `invoke Should throw an Exception when repository throws an Exception`() {
-        coEvery { repository.notes() } throws Exception()
+    fun `invoke Should throw an Exception when repository throws an Exception`() = runBlocking {
+        val expectedError = Exception()
+        val expectedResult = Result.failure<List<Note>>(expectedError)
+        coEvery { repository.notes() } throws expectedError
 
-        assertThrows(Exception::class.java) {
-            runBlocking { useCase() }
-        }
+        val result = useCase()
+
+        assertEquals(expectedResult, result)
     }
 
 }
