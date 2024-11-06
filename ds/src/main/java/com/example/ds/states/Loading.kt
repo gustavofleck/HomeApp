f65551f5dbca9036336adb5b9d69d4b57f5f0f57
Gustavo Carneiro Fleck <gustavocfleck@gmail.com>
2024-11-06 13:10:13 -0300

    FEAT: Add Loading state into DS Module

---
+++ ds/src/main/java/com/example/ds/states/Loading.kt
@@ -0,0 +1,25 @@
+package com.example.ds.states
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingState() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(64.dp),
+            strokeWidth = 8.dp
+        )
+    }
+}
